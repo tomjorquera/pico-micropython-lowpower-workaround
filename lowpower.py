298ada1832d2af3d52a10ef1be9c921549a8b30c
Tom Jorquera <tom@jorquera.net>
2021-04-06 18:23:28 +0200

    Add license file headers

--- lowpower.py
+++ lowpower.py
@@ -1,3 +1,19 @@
+# Raspberry Pico micropython low-power workaround
+# Copyright (C) 2021 Tom Jorquera
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 REG_IO_BANK0_BASE = 0x40014000
 REG_IO_BANK0_INTR0 = 0x0f0
 REG_IO_BANK0_DORMANT_WAKE_INTE0 = 0x160
